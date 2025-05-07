274ac297b670c53e5e9b519adff96964c8c66673
vnunez25 <vnunezbaro9886@sdsu.edu>
2025-05-06 23:41:41 -0700

    Switch to PostgreSQL for Render

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from datetime import datetime, timezone
 import random
 import os
 
-app = Flask(__name__)
+app = Flask(__name__) 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
